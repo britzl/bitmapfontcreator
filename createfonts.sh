368eebadd568170caf0e7227fd1c7993a5e52c10
Björn Ritzl <bjorn.ritzl@king.com>
2014-04-02 13:27:50 +0200

    Should not have been versioned

--- createfonts.sh
+++
@@ -1,4 +0,0 @@
-java -jar bitmapfontcreator.jar --ttf res-fonts/Futura.ttc --size 14 --glyphs glyphs_se.txt --antialias --color FFFFFFFF --out res-bitmapfont
-java -jar bitmapfontcreator.jar --ttf res-fonts/Futura.ttc --size 18 --glyphs glyphs_se.txt --antialias --color FFFFFFFF --out res-bitmapfont
-java -jar bitmapfontcreator.jar --ttf res-fonts/Futura.ttc --size 20 --glyphs glyphs_se.txt --antialias --color FFFFFFFF --out res-bitmapfont
-java -jar bitmapfontcreator.jar --ttf res-fonts/Futura.ttc --size 24 --glyphs glyphs_se.txt --antialias --color FFFFFFFF --out res-bitmapfont
